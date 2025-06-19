671989847547d365147b9e28badb8772407b2fd9
joevinM <joevin.manceau@hotmail.fr>
2025-06-19 14:53:33 +0000

    Task 2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-Entry point of the program
+ *
+ *This program prints all characters of alphabet.
+ *followed by a new line.
+ *
+ *Return:Always 0 (success)
+ */
+
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+	putchar (x);
+}
+putchar ('\n');
+	return (0);
+}
