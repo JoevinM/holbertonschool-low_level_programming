5eed490815d299915587610aa88a3489e4e02b1f
joevinM <joevin.manceau@hotmail.fr>
2025-06-19 15:52:20 +0000

    Task 4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,23 +2,20 @@
 /**
  *main-Entry point of the program
  *
- *This program prints all characters of alphabet without 'e' and 'q' characters,
- *followed by a new line.
+ *This program prints all characters of alphabet
+ *without 'e' and 'q' characters.
  *
  *Return:Always 0 (success)
  */
-
 int main(void)
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
-{
 /**
- * This function ignore 'e' and 'q' characters
+ * This function if ignore 'e' and 'q' characters
  */
 	if (x != 'e' && x != 'q')
 	putchar (x);
-}
 putchar ('\n');
 return (0);
 }
