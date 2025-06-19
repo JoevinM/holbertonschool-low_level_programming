b7115e2348344a05a2cfbb241e6dcffa22defc51
joevinM <joevin.manceau@hotmail.fr>
2025-06-19 21:32:59 +0000

    Task 6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-Entry point of the program
+ *
+ *This program prints all characters of alphabet.
+ *followed by a new line.
+ *
+ *Return:Always 0 (success)
+ */
+
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+	putchar (x);
+}
+putchar ('\n');
+	return (0);
+}
