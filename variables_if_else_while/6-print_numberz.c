c753a472efc89fa448f2b22511e4f58f2c933968
joevinM <joevin.manceau@hotmail.fr>
2025-06-19 23:36:18 +0200

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,8 +2,8 @@
 /**
  *main-Entry point of the program
  *
- *This program prints all characters of alphabet.
- *followed by a new line.
+ *This program prints all single digit numbers of base 10,
+ *starting from 0,followed by a new line.
  *
  *Return:Always 0 (success)
  */
