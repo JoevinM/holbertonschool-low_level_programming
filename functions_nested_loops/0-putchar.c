6fd2c946aa7a690086ebc1a1d490232927384002
joevinM <joevin.manceau@hotmail.fr>
2025-06-20 09:06:02 +0000

    Task 0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main-Entry point of the program
+  *
+  *This program print exactly '_putchar' followed by a new line.
+  *
+  *Return:Always 0 (success)
+  */
+
+int main(void)
+{
+	putchar (95);
+	putchar (112);
+	putchar (117);
+	putchar (116);
+	putchar (99);
+	putchar (104);
+	putchar (97);
+	putchar (114);
+	putchar ('\n');
+return (0);
+}
