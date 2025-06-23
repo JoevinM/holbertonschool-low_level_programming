2703ff89b849edd707e048f478eb7c5be36d4e0d
joevinM <joevin.manceau@hotmail.fr>
2025-06-23 13:40:22 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,18 @@
 
 void more_numbers(void)
 {
-int more;
-int x10;
+	int more;
+	int x10;
 
-while (x10 < 10)
-{
-	for (more = 0; more <= 14; more++)
+	while (x10 < 10)
 	{
-		if (more >= 10)
-			_putchar('0' + (more / 10));
-		_putchar('0' + (more % 10));
+		for (more = 0; more <= 14; more++)
+		{
+			if (more >= 10)
+				_putchar('0' + (more / 10));
+			_putchar('0' + (more % 10));
+		}
+		_putchar('\n');
+		x10++;
 	}
-	_putchar('\n');
-	x10++;
-}
 }
