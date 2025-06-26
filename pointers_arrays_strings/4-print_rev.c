e210f686ddeac04553bb938123c7bb5dc86ed14b
joevinM <joevin.manceau@hotmail.fr>
2025-06-26 14:53:44 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Print a string, in reversed, followed by a new line.
+ *
+ * @s: pointer to string in a array.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+	}
+	for (; i >=  0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
