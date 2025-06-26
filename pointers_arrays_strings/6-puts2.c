397f8064ee2019529c84d5bc24b73bc58dfd02d3
joevinM <joevin.manceau@hotmail.fr>
2025-06-26 16:01:27 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Print a string, 2 by 2 char followed by a new line.
+ *
+ * @str: pointer to string in a array.
+ *
+ */
+
+void puts2(char *str)
+
+{
+	int compteur = 0;
+
+	for (; str[compteur] != '\0'; compteur += 2)
+	{
+		_putchar(str[compteur]);
+	}
+	_putchar('\n');
+}
