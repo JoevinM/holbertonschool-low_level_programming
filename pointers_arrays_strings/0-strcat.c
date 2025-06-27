862859bc482c0cc15ef53949406b10e3f87b1d6d
joevinM <joevin.manceau@hotmail.fr>
2025-06-27 21:48:02 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,31 @@
 #include <stdio.h>
 
 /**
+ * _strcat - concatenates two strings.
+ * @dest: Destination string (will be modified).
+ * @src: Source string to append to dest.
  *
- *
- *
- *
+ * Return: Apointer to the resulting string dest.
  */
 
 char *_strcat(char *dest, char *src)
+{
+	int compteurDestination = 0;
+	int compteurSource = 0;
+	while (dest[compteurDestination] != '\0')
+	{
+		compteurDestination++;
+	}
+	while (src[compteurSource] != '\0')
+	{
+		dest[compteurDestination] = src[compteurSource];
+		compteurDestination++;
+		compteurSource++;
+	}
+	
+	dest[compteurDestination] = '\0';
 
+
+
+	return (dest);
+}
