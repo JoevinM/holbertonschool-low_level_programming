5ef4d0ae83dd0e16185e786c79990797fb5f217a
joevinM <joevin.manceau@hotmail.fr>
2025-07-01 21:17:22 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints the 9 times table, strating with 0.
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int multiply;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			multiply = j * i;
+			if (multiply >= 10)
+			{
+				_putchar('0' + (multiply / 10));
+				_putchar('0' + (multiply % 10));
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + multiply);
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
