feb7f6f4134e57cf68ff784ecc7736861b2c28e4
joevinM <joevin.manceau@hotmail.fr>
2025-07-01 08:49:53 +0000

    Betty style for 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,8 @@
  * @s: The string to be scanned.
  * @accept: The string containing the characters to match.
  *
- * Return: A pointer to the byte in @s that matches one of the bytes in @accept, or NULL if no such byte is found.
+ * Return: A pointer to the byte in @s that matches one
+ * of the bytes in @accept, or NULL if no such byte is found.
  */
 
 char *_strpbrk(char *s, char *accept)
