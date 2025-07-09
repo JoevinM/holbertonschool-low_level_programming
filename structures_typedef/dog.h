5d9b8c695557eb978edb1d2ed2d3f2ec398b6644
joevinM <joevin.manceau@hotmail.fr>
2025-07-09 15:56:10 +0200

    Task 0-dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - structure that defines a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: name of the dog's owner (string)
+ *
+ * Description: A structure that stores information about a dog,
+ * including its name, age, and the name of its owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
