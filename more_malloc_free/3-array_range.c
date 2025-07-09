0f61545b1dc83768dbabbfb3a89819425f3adc83
joevinM <joevin.manceau@hotmail.fr>
2025-07-09 14:38:53 +0200

    Fix programm 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,16 +20,16 @@
 int *array_range(int min, int max)
 {
 	int *array;
-	int i = 0;
+	int i;
 
 	if (min > max)
 		return (NULL);
 
 	array = malloc(sizeof(int) * ((max - min) + 1));
-		if (array == NULL)
-			return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-	for (; i <= max ; i++)
+	for (i = 0; i <= (max - min); i++)
 		array[i] = min + i;
 
 	return (array);
