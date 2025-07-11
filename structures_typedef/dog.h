999bed43bc703583b1eb36c30244e9a533258978
joevinM <joevin.manceau@hotmail.fr>
2025-07-11 13:51:30 +0200

    Task 3 Dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,14 +13,15 @@
  * including its name, age, and the name of its owner.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 #endif /* DOG_H */
