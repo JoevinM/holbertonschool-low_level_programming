a7649e295fc9eb2f07d7e8ebb26814a2104b9fe7
joevinM <joevin.manceau@hotmail.fr>
2025-07-18 14:26:04 +0200

    Betty style All files fix

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,13 +1,12 @@
 #ifndef CALC_H
 #define CALC_H
 
- /**
+/**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -21,6 +20,5 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
-
 
 #endif
