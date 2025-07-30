c580a673f7ec5c48ffd8cdcebdfe7260bce71d0d
joevinM <joevin.manceau@hotmail.fr>
2025-07-30 14:36:01 +0200

    Betty style for add_dnodeint_end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,6 +2,20 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list.
+ * @head: Double pointer to the head of the list.
+ * @n: Integer value to store in the new node.
+ *
+ * Return: Address of the new node, or NULL if memory allocation fails.
+ *
+ * Description:
+ * - Allocates a new node and sets its value.
+ * - If the list is empty, the new node becomes the head.
+ * - Otherwise, traverses to the last node and appends the new node.
+ * - Updates the previous pointer of the new node to link it properly.
+ */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
@@ -14,7 +28,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	new->prev = *head;
 
 	if (*head == NULL)
 	{
@@ -28,6 +41,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		temp = temp->next;
 
 	temp->next = new;
-
+	new->prev = temp;
 	return (new);
 }
