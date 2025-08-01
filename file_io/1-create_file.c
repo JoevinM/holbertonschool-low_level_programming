fab7f533cdb528d560bcda9f9fa65d5d2d637161
joevinM <joevin.manceau@hotmail.fr>
2025-08-01 14:20:41 +0200

    Add a description to create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,6 +3,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * create_file - Creates a file and writes text to it
+ * @filename: Name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ *
+ * Description:
+ * - If the file exists, it is truncated.
+ * - If it doesn’t exist, it is created with permissions rw------- (0600).
+ * - If text_content is NULL, an empty file is created.
+ * - Returns -1 if filename is NULL, or if file cannot be created or written.
+ */
 
 int create_file(const char *filename, char *text_content)
 {
