7b9eb1556ce2ac2fa2fbee812aaf8af46f89754b
joevinM <joevin.manceau@hotmail.fr>
2025-08-01 14:09:08 +0200

    Fix the (-1) return

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,11 +9,11 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor, bytes_written, i = 0;
 
 	if (filename == NULL)
-		return (-12);
+		return (-1);
 
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor == -1)
-		return (-13);
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		if (bytes_written == -1 || bytes_written != i)
 		{
 			close(file_descriptor);
-			return (-15);
+			return (-1);
 		}
 	}
 	close(file_descriptor);
